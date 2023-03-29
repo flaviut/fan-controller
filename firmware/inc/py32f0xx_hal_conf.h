699800aef1dd9478dcb50fe9b907d472599268b1
Flaviu Tamas <me@flaviutamas.com>
2023-03-28 21:13:34 -0400

    Update pre-commit

--- firmware/inc/py32f0xx_hal_conf.h
+++ firmware/inc/py32f0xx_hal_conf.h
@@ -25,7 +25,7 @@
 #define __PY32F0xx_HAL_CONF_H
 
 #ifdef __cplusplus
- extern "C" {
+extern "C" {
 #endif
 
 /* Exported types ------------------------------------------------------------*/
@@ -55,28 +55,28 @@
 
 /* ########################## Oscillator Values adaptation ####################*/
 
-#if !defined  (HSI_VALUE)
-  #define HSI_VALUE              ((uint32_t)8000000)     /*!< Value of the Internal oscillator in Hz */
-#endif /* HSI_VALUE */
+#if !defined(HSI_VALUE)
+#define HSI_VALUE ((uint32_t) 8000000) /*!< Value of the Internal oscillator in Hz */
+#endif                                 /* HSI_VALUE */
 
 /**
   * @brief Adjust the value of External High Speed oscillator (HSE) used in your application.
   *        This value is used by the RCC HAL module to compute the system frequency
   */
-#if !defined  (HSE_VALUE)
-  #define HSE_VALUE              ((uint32_t)24000000) /*!< Value of the External oscillator in Hz */
-#endif /* HSE_VALUE */
+#if !defined(HSE_VALUE)
+#define HSE_VALUE ((uint32_t) 24000000) /*!< Value of the External oscillator in Hz */
+#endif                                  /* HSE_VALUE */
 
-#if !defined  (HSE_STARTUP_TIMEOUT)
-  #define HSE_STARTUP_TIMEOUT    ((uint32_t)200)   /*!< Time out for HSE start up, in ms */
-#endif /* HSE_STARTUP_TIMEOUT */
+#if !defined(HSE_STARTUP_TIMEOUT)
+#define HSE_STARTUP_TIMEOUT ((uint32_t) 200) /*!< Time out for HSE start up, in ms */
+#endif                                       /* HSE_STARTUP_TIMEOUT */
 
 /**
   * @brief Internal Low Speed Internal oscillator (LSI) value.
   */
-#if !defined  (LSI_VALUE)
- #define LSI_VALUE               ((uint32_t)32768)    /*!< LSI Typical Value in Hz */
-#endif /* LSI_VALUE */                                /*!< Value of the Internal Low Speed oscillator in Hz
+#if !defined(LSI_VALUE)
+#define LSI_VALUE ((uint32_t) 32768) /*!< LSI Typical Value in Hz */
+#endif /* LSI_VALUE */               /*!< Value of the Internal Low Speed oscillator in Hz
                                                       The real value may vary depending on the variations
                                                       in voltage and temperature. */
 
@@ -84,13 +84,13 @@
   * @brief Adjust the value of External Low Speed oscillator (LSE) used in your application.
   *        This value is used by the RCC HAL module to compute the system frequency
   */
-#if !defined  (LSE_VALUE)
-  #define LSE_VALUE              ((uint32_t)32768) /*!< Value of the External oscillator in Hz*/
-#endif /* LSE_VALUE */
+#if !defined(LSE_VALUE)
+#define LSE_VALUE ((uint32_t) 32768) /*!< Value of the External oscillator in Hz*/
+#endif                               /* LSE_VALUE */
 
-#if !defined  (LSE_STARTUP_TIMEOUT)
-  #define LSE_STARTUP_TIMEOUT    ((uint32_t)5000)   /*!< Time out for LSE start up, in ms */
-#endif /* LSE_STARTUP_TIMEOUT */
+#if !defined(LSE_STARTUP_TIMEOUT)
+#define LSE_STARTUP_TIMEOUT ((uint32_t) 5000) /*!< Time out for LSE start up, in ms */
+#endif                                        /* LSE_STARTUP_TIMEOUT */
 
 /* Tip: To avoid modifying this file each time you need to use different HSE,
    ===  you can define the HSE value in your toolchain compiler preprocessor. */
@@ -99,14 +99,14 @@
 /**
   * @brief This is the HAL system configuration section
   */
-#define  VDD_VALUE               ((uint32_t)3300) /*!< Value of VDD in mv */
-#define  PRIORITY_HIGHEST        0
-#define  PRIORITY_HIGH           1
-#define  PRIORITY_LOW            2
-#define  PRIORITY_LOWEST         3
-#define  TICK_INT_PRIORITY       ((uint32_t)PRIORITY_LOWEST)    /*!< tick interrupt priority (lowest by default)  */
-#define  USE_RTOS                0
-#define  PREFETCH_ENABLE         0
+#define VDD_VALUE ((uint32_t) 3300) /*!< Value of VDD in mv */
+#define PRIORITY_HIGHEST 0
+#define PRIORITY_HIGH 1
+#define PRIORITY_LOW 2
+#define PRIORITY_LOWEST 3
+#define TICK_INT_PRIORITY ((uint32_t) PRIORITY_LOWEST) /*!< tick interrupt priority (lowest by default)  */
+#define USE_RTOS 0
+#define PREFETCH_ENABLE 0
 
 /* ########################## Assert Selection ############################## */
 /**
@@ -121,33 +121,33 @@
   * @brief Include module's header file
   */
 #ifdef HAL_MODULE_ENABLED
- #include "py32f0xx_hal.h"
+#include "py32f0xx_hal.h"
 #endif /* HAL_MODULE_ENABLED */
 
 // #include "py32f0xx_hal_dma.h"
 
 #ifdef HAL_RCC_MODULE_ENABLED
- #include "py32f0xx_hal_rcc.h"
+#include "py32f0xx_hal_rcc.h"
 #endif /* HAL_RCC_MODULE_ENABLED */
 
 #ifdef HAL_EXTI_MODULE_ENABLED
- #include "py32f0xx_hal_exti.h"
+#include "py32f0xx_hal_exti.h"
 #endif /* HAL_EXTI_MODULE_ENABLED */
 
 #ifdef HAL_GPIO_MODULE_ENABLED
- #include "py32f0xx_hal_gpio.h"
+#include "py32f0xx_hal_gpio.h"
 #endif /* HAL_GPIO_MODULE_ENABLED */
 
 #ifdef HAL_CORTEX_MODULE_ENABLED
- #include "py32f0xx_hal_cortex.h"
+#include "py32f0xx_hal_cortex.h"
 #endif /* HAL_CORTEX_MODULE_ENABLED */
 
 #ifdef HAL_ADC_MODULE_ENABLED
- #include "py32f0xx_hal_adc.h"
+#include "py32f0xx_hal_adc.h"
 #endif /* HAL_ADC_MODULE_ENABLED */
 
 #ifdef HAL_CRC_MODULE_ENABLED
- #include "py32f0xx_hal_crc.h"
+#include "py32f0xx_hal_crc.h"
 #endif /* HAL_CRC_MODULE_ENABLED */
 
 #ifdef HAL_COMP_MODULE_ENABLED
@@ -155,43 +155,43 @@
 #endif /* HAL_COMP_MODULE_ENABLED */
 
 #ifdef HAL_FLASH_MODULE_ENABLED
- #include "py32f0xx_hal_flash.h"
+#include "py32f0xx_hal_flash.h"
 #endif /* HAL_FLASH_MODULE_ENABLED */
 
 #ifdef HAL_I2C_MODULE_ENABLED
- #include "py32f0xx_hal_i2c.h"
+#include "py32f0xx_hal_i2c.h"
 #endif /* HAL_I2C_MODULE_ENABLED */
 
 #ifdef HAL_IWDG_MODULE_ENABLED
- #include "py32f0xx_hal_iwdg.h"
+#include "py32f0xx_hal_iwdg.h"
 #endif /* HAL_IWDG_MODULE_ENABLED */
 
 #ifdef HAL_PWR_MODULE_ENABLED
- #include "py32f0xx_hal_pwr.h"
+#include "py32f0xx_hal_pwr.h"
 #endif /* HAL_PWR_MODULE_ENABLED */
 
 #ifdef HAL_SPI_MODULE_ENABLED
- #include "py32f0xx_hal_spi.h"
+#include "py32f0xx_hal_spi.h"
 #endif /* HAL_SPI_MODULE_ENABLED */
 
 #ifdef HAL_TIM_MODULE_ENABLED
- #include "py32f0xx_hal_tim.h"
+#include "py32f0xx_hal_tim.h"
 #endif /* HAL_TIM_MODULE_ENABLED */
 
 #ifdef HAL_LPTIM_MODULE_ENABLED
- #include "py32f0xx_hal_lptim.h"
+#include "py32f0xx_hal_lptim.h"
 #endif /* HAL_LPTIM_MODULE_ENABLED */
 
 #ifdef HAL_UART_MODULE_ENABLED
- #include "py32f0xx_hal_uart.h"
+#include "py32f0xx_hal_uart.h"
 #endif /* HAL_UART_MODULE_ENABLED */
 
 #ifdef HAL_USART_MODULE_ENABLED
- #include "py32f0xx_hal_usart.h"
+#include "py32f0xx_hal_usart.h"
 #endif /* HAL_USART_MODULE_ENABLED */
 
 /* Exported macro ------------------------------------------------------------*/
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
   * @brief  The assert_param macro is used for function's parameters check.
   * @param  expr: If expr is false, it calls assert_failed function
@@ -200,11 +200,11 @@
   *         If expr is true, it returns no value.
   * @retval None
   */
-  #define assert_param(expr) ((expr) ? (void)0U : assert_failed((uint8_t *)__FILE__, __LINE__))
+#define assert_param(expr) ((expr) ? (void) 0U : assert_failed((uint8_t *) __FILE__, __LINE__))
 /* Exported functions ------------------------------------------------------- */
-  void assert_failed(uint8_t* file, uint32_t line);
+void assert_failed(uint8_t *file, uint32_t line);
 #else
-  #define assert_param(expr) ((void)0U)
+#define assert_param(expr) ((void) 0U)
 #endif /* USE_FULL_ASSERT */
 
 #ifdef __cplusplus
