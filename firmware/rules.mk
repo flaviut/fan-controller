8363b63ade91f3edaae544ec184aac6e66882670
Flaviu Tamas <me@flaviutamas.com>
2023-06-19 20:47:17 -0400

    Avoid erasing flash before writing
    There's already a system to avoid flashing blocks that haven't changed,
    so there's not reason to spend longer unnecessary

--- firmware/rules.mk
+++ firmware/rules.mk
@@ -118,7 +118,6 @@ flash: $(BDIR)/$(PROJECT).hex
 ifeq ($(FLASH_PROGRM),jlink)
 	$(JLINKEXE) -device $(JLINK_DEVICE) -if swd -speed 4000 -JLinkScriptFile $(TOP)/Misc/jlink-script -CommanderScript $(TOP)/Misc/jlink-command
 else ifeq ($(FLASH_PROGRM),pyocd)
-	$(PYOCD_EXE) erase -t $(PYOCD_DEVICE) --chip --config $(TOP)/Misc/pyocd.yaml
 	$(PYOCD_EXE) load $(BDIR)/$(PROJECT).hex -t $(PYOCD_DEVICE) --config $(TOP)/Misc/pyocd.yaml
 else
 	@echo "FLASH_PROGRM is invalid\n"
